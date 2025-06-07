7d9bf74357df111db8ce85b91fa3c98b19c595f1
Benny Lenny <bennettleonard42@gmail.com>
2025-06-07 12:39:33 +0200

    build basic card component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,42 @@
+'use client';
+
+import {Card} from "@/components/Card";
 
 function Page() {
   return (
-    <h1 className="text-3xl font-bold mb-4">
-      Hello World!
-    </h1>
+    <section className="container mx-auto">
+      <h1 className="text-3xl font-bold mb-4 bg-red-500">
+        About
+      </h1>
+      <p className="font-bold mb-4">
+        Hello! I’m Bennett and you’re probably wondering whats going on here - me too. ¯\_(ツ)_/¯
+      </p>
+      <p className="mb-4">
+        Lets start from the beginning: 13.8 billion years ag--
+      </p>
+      <p className="mb-4">
+        Lets cut to the chase: I grew up in Idaho, followed some summoning for adventures abroad, and am currently based in Amsterdam where I have lived for the past couple years but what has felt already like a lifetimes worth of lessons - and expected no less living on a saturn line.
+      </p>
+      <p className="mb-4">
+        I’ve watched friends come and go, interests come and go, identities come and go and as the years go by, I am gaining more confidence in my ability to “handle the seasons of my life” (Landslide, Fleetwood mac)
+      </p>
+      <p className="mb-4">
+        I am working as a software engineer at Lovevery but when I'm not working I can be found gardening, sleeping, sketching, making cocktails or doing nothing which is becoming a favorite pastime of mine
+      </p>§§
+
+      <div className="flex gap-2">
+        <Card front="Studying" back="Herbalism, Palmistry, Mixology, Astrology" />
+
+        <Card front="Watching" back="White Lotus, Taskmaster, One Piece" />
+
+        <Card front="Playing" back="Disney Dreamlight Valley" />
+
+        <Card front="Reading" back="Be Here Now, Surviving Saturn Return, American Fascism" />
+
+        <Card front="Wearing" back="Bombas Slippers || Docs" />
+      </div>
+
+    </section>
   );
 }
 
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+type CardProps = {
+  front: string;
+  back: string;
+}
+
+const Card = ({ front, back }: CardProps) => {
+  const [isFlipped, setIsFlipped] = useState<boolean>(false);
+
+  // todo figure out how to do a transition on width when flipped
+  // todo maybe back is tab type component
+  return (
+    <div data-testid="card" onClick={() => setIsFlipped(!isFlipped)} className="cursor-pointer p-4 w-fit border-amber-400 border-2 rounded-md">
+      <p>{isFlipped ? back : front}</p>
+    </div>
+  );
+}
+
+export default Card;
