b818b4ff8efc69615b00df68d8d51c24ca82b61f
Benny Lenny <bennettleonard42@gmail.com>
2026-09-12 13:44:32 +0200

    update footer icon, year

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,11 @@
-import { Github } from 'lucide-react';
+import { Code } from 'lucide-react';
 
 import { Link } from "@/components";
 import { LinkStyle } from "@/components/Link/Link";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear().toString();
+
   return (
     <footer className="p-4 text-center text-sm text-light bg-secondary">
       <div className="flex flex-col md:flex-row items-center justify-between gap-2 md:gap-6">
@@ -13,12 +15,12 @@ const Footer = () => {
           style={LinkStyle.Light}
           isExternal
         >
-          <Github size={24} />
+          <Code size={24} />
           <p>Source code</p>
         </Link>
 
         <p className="text-center">
-          © 2025 www.bennylenny.dev. All rights reserved.
+          © 2025-{currentYear} www.bennylenny.dev. All rights reserved.
         </p>
       </div>
     </footer>
